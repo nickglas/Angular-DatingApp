213efe2969ff17b54205e7dc3fe5818d18691a40
nickglas <40456410+nickglas@users.noreply.github.com>
2021-02-14 00:29:22 +0100

    models and user service

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { from } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
-  baseurl = "http://localhost:5000/api/auth/";
+  baseurl = environment.apiUrl + "auth/";
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
